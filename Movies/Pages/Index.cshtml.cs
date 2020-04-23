fd36e2795a34f02b970a8510ae565f9907c6f34a
May Phyo <maytphyo@gmail.com>
2020-04-22 20:06:09 -0500

    Search and Filtering Done

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -10,9 +10,58 @@ namespace Movies.Pages
     public class IndexModel : PageModel
     {
 
-        public void OnGet()
+        /// <summary>
+        /// Gets and sets movies
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
+
+        /// <summary>
+        /// Gets and sets search terms
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        ///  Gets and sets the MPAA ratings
+        /// </summary>
+        [BindProperty]
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// Gets and sets the genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// Gets and sets min IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// Gets and sets Max IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
+
+
+        /// <summary>
+        /// Initializes incoming GET requests
+        /// </summary>
+        public void OnGet(double? IMDBMin, double? IMDBMax)
         {
+            // Nullable conversion workaround
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
+            MPAARatings = Request.Query["MPAARatings"];
+            Genres = Request.Query["Genres"];
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
 
         }
     }
+
 }
